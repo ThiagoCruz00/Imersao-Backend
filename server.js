bfa3e543ae1c43fc03f5e25a0d8ba60d4a58bbd7
Thiago <thiagohfs.cruz@gmail.com>
2024-11-26 20:17:09 -0300

    feat: Criei uma API

--- server.js
+++ server.js
@@ -1,10 +1,35 @@
 import express from 'express';
 
+const posts = [
+    {
+        id: 1,
+        descricao: 'Uma foto teste',
+        imagem: 'https://playcats.com/millie/300/150'
+    },
+    {
+        id: 2,
+        descricao: 'Um lindo pôr do sol na praia',
+        imagem: 'https://playcats.com/millie/300/150'
+    },
+];
 const app = express();
+app.use(express.json())
+
 app.listen(3000, () => {
     console.log("Servidor escutando...");
 });
 
-app.get("/api", (req, res) => {
-    res.status(200) .send("Boas vindas a imersão!");
+app.get("/posts", (req, res) => {
+    res.status(200).json(posts);
+});
+
+
+function BuscarPosts(Id){
+    return posts.findIndex((posts) => {
+        return posts.id === Number(Id)
+    })
+}
+app.get("/posts/:id", (req, res) => {
+    const index = BuscarPosts(req.params.id)
+    res.status(200).json(posts[index]);
 });
